8c17fff1dddceda2537f761cc1f6c315aaf1a3b6
Florian O <fosterme@hm.edu>
2025-01-23 11:28:40 +0100

    Aktualisiere die Pfade in log_player_activity.sh für die Protokollierung der Spieleraktivität

--- log_player_activity.sh
+++ log_player_activity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Pfade und Variablen
-MINECRAFT_LOG_DIR="/mcs/mc_server/logs/"
-OUTPUT_LOG="/mcs/player_activity.log"
+MINECRAFT_LOG_DIR="/home/pi/mcs/mc_server/logs"
+OUTPUT_LOG="/home/pi/mcs/player_activity.log"
 
 # Überprüfen, ob das Output-Log existiert, ansonsten erstellen
 if [ ! -f "$OUTPUT_LOG" ]; then
@@ -15,11 +15,13 @@ tail -F "$MINECRAFT_LOG_DIR/latest.log" | while read LINE
 do
     # Spieler-Login erfassen
     if echo "$LINE" | grep -q "joined the game"; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $(echo "$LINE" | awk '{print $5}') joined the game" >> "$OUTPUT_LOG"
+        PLAYER=$(echo "$LINE" | awk '{print $5}')
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - $PLAYER joined the game" >> "$OUTPUT_LOG"
     fi
 
     # Spieler-Logout erfassen
     if echo "$LINE" | grep -q "left the game"; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $(echo "$LINE" | awk '{print $5}') left the game" >> "$OUTPUT_LOG"
+        PLAYER=$(echo "$LINE" | awk '{print $5}')
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - $PLAYER left the game" >> "$OUTPUT_LOG"
     fi
 done
